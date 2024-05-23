0301d8bcef917eba759f16caf77f6f6e0655488c
MohammedGayaz <sheikgayazuddin@gmail.com>
2024-05-23 12:57:33 +0530

    remove nodemodules dist

--- common/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { registerInput } from "./auth/registerInput";
-export { loginInput } from "./auth/loginInput";
-export { createBlogInput } from "./blog/createBlogInput";
-export { updateBlogInput } from "./blog/updateBlogInput";
